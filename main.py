74cd4e54051dc8a1a5824665524a61b328225e1c
josephchait <danchait10@gmail.com>
2022-05-05 04:23:20 -0400

    still needs do_power function

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def do_power():
+    #Help me Yehuda!!!
+    print("Help me Yehuda!")
+
+
+#this is a program that gets 2 numbers and returns one in the power of the other.
+print("This is a program that gets 2 numbers and returns one in the power of the other.")
+
+#get numbers
+first_num = input("Please input the first number:")
+second_num = input("Please input the second number:")
+
+print(do_power(first_num, second_num))
+
